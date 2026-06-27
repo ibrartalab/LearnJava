54660783235c5aca226d762ca9a93f5d6d735afe
ibrar talab <ibrartalabkhan@gmail.com>
2026-06-27 19:11:07 +0500

    Rewrite ConditionalStatement with explanation guides. What is ternary operators and where should we use it.
    Signed-off-by: ibrar talab <ibrartalabkhan@gmail.com>

--- src/coreJava/conditionalStatments/ConditionalStatement.java
+++ src/coreJava/conditionalStatments/ConditionalStatement.java
@@ -1,89 +1,43 @@
-void main() {
-    // To work with conditional statements we first need to understand some operators.
-    // Relational Operators - Operands string,char,short,int,long
-    // in relational operators we have <, >, <=, >=, ==, !=
-    int a = 100;
-    int b = 200;
-    boolean c = a > b; // false
-    boolean c1 = a < b; // true
-    boolean c2 = a <= b; // true
-    boolean c3 = a >= b; // false
-    boolean c4 = a == 100; // true
-    boolean c5 = a != b; // true
-    System.out.println(c5);
+package coreJava.conditionalStatments;
 
-    // Logical operators -> &&, ||, !
-    // Logical AND && operator
-    String jobPost = "Senior Software Engineer";
-    //Requirements for this job role
-    int minYearOfExp = 5;
-    String degreeIn = "CS";
-    int userExp = 4;
-    String userDegree = "BBA";
-    if(userExp >= minYearOfExp && userDegree.equals(degreeIn)){
-        System.out.printf("You can apply for the job role %s",jobPost);
-        System.out.println();
-    }else{
-        System.out.printf("You are not currently eligible for this job role %s",jobPost);
-        System.out.println();
+public class ConditionalStatement {
+    static void main() {
+        // To work with conditional statements we first need to understand some operators.
+        // Relational Operators like <(less than), >(greater than), ==(equal to),>=,<= and !(not equal to)
+        // Logical operators and ternary operators as well.
+        // We could use conditionals with these operands string,char,short,int,long
+        // Operands basically are the actual data on which you could perform operations (e.g) 2 > 3
+
+        // A good scenario where you are the hiring manager for CodeJava company(a dummy name used here) and you have been
+        // some certain rules for the job role. Like minimum of five years of experience and must have a cs(computer science) degree.
+        String jobRole = "Mid Software Engineer";
+        int minExperience = 2;
+        String degreeOfStudying = "Arts";
+        // so in this scenario both conditions are must have.
+        if(minExperience >= 5 && degreeOfStudying == "CS"){
+            System.out.println("Congrats! You are eligible for this opportunity " + jobRole);
+        }else{
+            System.out.println("Oh! try next time.");
+        }
+
+        // Creating a student grading system to utilize all relational, logical operators with conditional statement
+        // Criteria:
+        // Marks > 90 "A", >= 75 "B", >= 60 "C", < 60 fail
+        int marks = 90;
+        if(marks >= 90){
+            System.out.println("Congrats! You have secured a Grade A");
+        }else if(marks >= 75){
+            System.out.println("Congrats! You have secured a Grade B");
+        }else if(marks >= 60){
+            System.out.println("Congrats! You have secured a Grade C");
+        }else{
+            System.out.println("Sorry! You are failed.");
+        }
+
+        // Now is we discussed earlier that there is an optimize/ one line feature to replace writing if-else statements multiple times.
+        int openTime = 10;
+        int closingTime = 15;
+        boolean isOpen = openTime < closingTime ? true : false; // ternary operators
+        System.out.println(isOpen);
     }
-
-    // Logial OR || operator
-    String jobPost2 = "Internee Software Engineer";
-    String degreeOfStudying = "CS";
-    switch (degreeOfStudying){
-        case "CS","SE","IT":
-            System.out.printf("You can apply for this internship opportunity for the role of %s",jobPost2);
-            System.out.println();
-            break;
-    }
-
-    // Logical NOT ! operator
-    boolean notEligible = false;
-    System.out.println(!notEligible);
-    // Converts true into false and false into true.
-
-    // Creating a student grading system to utilize all relational, logical operators with conditional statement
-    // Criteria:
-    // Marks > 90 "A", >= 75 "B", >= 60 "C", < 60 fail
-    int marks = 90;
-    if(marks >= 90){
-        System.out.println("Congrats! You have secured a Grade A");
-    }else if(marks >= 75){
-        System.out.println("Congrats! You have secured a Grade B");
-    }else if(marks >= 60){
-        System.out.println("Congrats! You have secured a Grade C");
-    }else{
-        System.out.println("Sorry! You are failed.");
-    }
-
-    // But there is an optimize way of doing this
-    // Switch case
-    int dayNumber = 1;
-    switch(dayNumber){
-        case 1:
-            System.out.println("Monday!");
-            break;
-        case 2:
-            System.out.println("Tuesday!");
-            break;
-        case 3:
-            System.out.println("Wednesday!");
-            break;
-        case 4:
-            System.out.println("Thursday!");
-            break;
-        case 5:
-            System.out.println("Friday!");
-            break;
-        case 6:
-            System.out.println("Saturday!");
-            break;
-        case 7:
-            System.out.println("Sunday!");
-            break;
-        default:
-            System.out.println("Invalid Entry.");
-    }
-
 }
