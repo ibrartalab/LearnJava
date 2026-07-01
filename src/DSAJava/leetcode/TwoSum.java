a045104927682530ce2830bc2bbd00aaf73f2c91
ibrar talab <ibrartalabkhan@gmail.com>
2026-07-01 20:18:29 +0500

    Add source code for TwoSum problem.
    Signed-off-by: ibrar talab <ibrartalabkhan@gmail.com>

--- src/DSAJava/leetcode/TwoSum.java
+++ src/DSAJava/leetcode/TwoSum.java
@@ -1,5 +1,24 @@
 package DSAJava.leetcode;
 
 public class TwoSum {
+    static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] resultIndexes = TwoSum.findTwoSum(nums,target);
+        for (int index:resultIndexes){
+            System.out.print(index + " ");
+        }
+    }
 
+    // The Brute-Force(BF) solution
+    public static int[] findTwoSum(int[] nums, int target) {
+        for(int i=0; i < nums.length; i++){
+            for(int j=i+1; j < nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[] {-1,-1};
+    }
 }
