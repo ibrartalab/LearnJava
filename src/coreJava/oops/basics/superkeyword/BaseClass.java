4202166af23d7b452d50710cf3b749b8ecbf863d
ibrar talab <ibrartalabkhan@gmail.com>
2026-07-08 12:02:39 +0500

    Add super keyword how to use it and when.
    Signed-off-by: ibrar talab <ibrartalabkhan@gmail.com>

---
+++ src/coreJava/oops/basics/superkeyword/BaseClass.java
@@ -0,0 +1,12 @@
+package coreJava.oops.basics.superkeyword;
+
+public class BaseClass {
+    public  String name = "Base class";
+    public  int age = 20;
+    public  int number = 101;
+
+    public BaseClass(String name){
+        this.name = name;
+        System.out.println(name);
+    }
+}
---
+++ src/coreJava/oops/basics/superkeyword/SubClass.java
@@ -0,0 +1,23 @@
+package coreJava.oops.basics.superkeyword;
+
+public class SubClass extends BaseClass {
+    // super keyword helps us to call the methods,properties and constructor of
+    // the super/parent class inside the sub/child class
+
+    private int age;
+//    private  String name;
+
+    public SubClass(int age,String name){
+        this.age = age;
+        super(name);
+    }
+
+    public int getBaseClassAge(){
+        return  super.age;
+    }
+
+    static void main(String[] args) {
+        SubClass sb = new SubClass(23,"subclass");
+        System.out.println(sb.getBaseClassAge());
+    }
+}
