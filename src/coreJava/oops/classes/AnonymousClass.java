ed3c80511c7c1242dddffc4690c17be6f7684d00
ibrar talab <ibrartalabkhan@gmail.com>
2026-07-10 22:21:08 +0500

    Add anonymous classes from outer classes and by implementing interfaces.
    Signed-off-by: ibrar talab <ibrartalabkhan@gmail.com>

---
+++ src/coreJava/oops/classes/AnonymousClass.java
@@ -0,0 +1,52 @@
+package coreJava.oops.classes;
+
+public class AnonymousClass {
+    static void main() {
+        /*
+         * This is the way of creating anonymous subClass of the outer class.
+         * Anonymous class is the same is inner or static subclass in terms of creating,
+         * But it can only be used once.
+         * In some scenario where you want to create a subclass, the use of it will be once,
+         * then in that case instead of creating inner or nested static subclasses you can
+         * use anonymous class.
+         */
+        OuterClass aClass = new OuterClass(){
+            /*
+             * we can define methods and properties as much is we want like
+             * a normal regular classes. It is the same as others classes, but it does
+             * not have a name.
+             */
+            public void show(){
+              System.out.println("This will show something!");
+            }
+
+            // We could also override method of the outer class.
+             @Override
+            public void play(){
+                 System.out.println("Anonymous is playing something!");
+             }
+
+        };
+
+        OuterInterface aInterface = new OuterInterface() {
+            @Override
+            public void sing() {
+                System.out.println("Anonymous interface is singing.");
+            }
+        };
+
+        aInterface.sing();
+        aClass.play();
+
+    }
+}
+
+class OuterClass{
+    public void play(){
+        System.out.println("This will play something!");
+    }
+}
+
+interface OuterInterface{
+    void sing();
+}
