f4565eafee514acc71f1d81997d3d0ffd81051ae
ibrar talab <ibrartalabkhan@gmail.com>
2026-07-13 18:06:11 +0500

    Intro to Java Collection Framework(JCF).
    Signed-off-by: ibrar talab <ibrartalabkhan@gmail.com>

---
+++ src/coreJava/collections/Intro.java
@@ -0,0 +1,19 @@
+package coreJava.collections;
+
+public class Intro {
+    /*
+     The Java Collection Framework(JCF) is a built-in set of ready-mad
+     classes and interfaces that helps you to store,organize, and manipulate group of objects
+     efficiently.
+     Instead of writing complex data-handling code from scratch(building a custom resizing array
+     or a linked list), Java provides these pre-built tools in the java.util.package.
+
+     As we know data structures are the containers which hold/store different types of data.
+     So, the JCF works on 4 main types of containers(data structures).
+     List(The ordered line)
+     Set(The unique container: just hold unique elements)
+     Queue(The waiting line: Use the FIFO(first in, first out) principle.
+     Map(The ID directory: stores data in a key-value pair).
+     */
+
+}
