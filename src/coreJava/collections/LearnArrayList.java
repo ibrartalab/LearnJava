0d6fc0a3c63b00aa652caaf9c8bc1891d3da2963
ibrar talab <ibrartalabkhan@gmail.com>
2026-07-14 21:43:49 +0500

    Learn ArrayList and how to use some methods to work with ArrayList. add(), get(), size(), addAll(), and addAll(i,Collection) the own implementation of the List interface.
    Signed-off-by: ibrar talab <ibrartalabkhan@gmail.com>

---
+++ src/coreJava/collections/LearnArrayList.java
@@ -0,0 +1,80 @@
+package coreJava.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LearnArrayList {
+    static void main(String[] args) {
+        /*
+         Before actually working with ArrayList, we know that ArrayList is implemented List(Interface).
+         List(Interface) extends Collection(Interface) extends iterable(Interface).
+         So, we can use all the implementations of the Collection and Iterable interface.
+         We will look it one by one.
+        */
+        List<String> cities = new ArrayList<>();
+        /*
+         Let's try the List interface methods.
+         Some of these methods are extended from their parent interfaces.
+         add is an abstract method of Collection interface and overridden by many others interface like List.
+         List interface support duplicates entries and stored in sequential ordered.
+
+         add() appends element to the end of the List.
+         It is also throws some exceptions on a certain scenarios.
+        */
+        cities.add("Islamabad");
+        cities.add("Karachi");
+        cities.add("Lahore");
+        cities.add("Peshawar");
+        cities.add("Peshawar");
+        System.out.println(cities);
+
+        // get() return the element on the specified index.
+        System.out.println(cities.get(3));
+
+        // size() return the length or numbers of elements in the list.
+        System.out.println(cities.size());
+
+        /*
+         addALl() will add a list of elements to the other list
+         adding one list inside another one.
+        */
+        List<String> cities1 = new ArrayList<>();
+        cities1.add("Swat");
+        cities1.add("Quetta");
+        cities1.add("Abbottabad");
+        System.out.println(cities.addAll(cities1));
+        /*
+         Now cities List will contain the following elements.
+         [Islamabad, Karachi, Lahore, Peshawar, Peshawar, Swat, Quetta, Abbottabad]
+        */
+        System.out.println(cities);
+
+        /*
+         There is one more overloaded addAll() method, which is accepting two
+         params the index(from where to start adding the new list element),the new
+         list.
+         Think of adding two Char list in the correct way.
+         list1 = ['A','B','C','H']
+         list2 = ['D','E','F','G']
+         To maintain the correct order we should need to be adding the list2 elements
+         after the second last element of the list1 like after the 'C'.
+         So, in this case we should use the addAll() the List interface method, not the
+         one which is coming from the Collection interface.
+         [A, B, C, D, E, F, G, H]
+        */
+        List<Character> alpha = new ArrayList<>();
+        alpha.add('A');
+        alpha.add('B');
+        alpha.add('C');
+        alpha.add('H');
+
+        List<Character> alpha1 = new ArrayList<>();
+        alpha1.add('D');
+        alpha1.add('E');
+        alpha1.add('F');
+        alpha1.add('G');
+
+        System.out.println(alpha.addAll(3,alpha1));
+        System.out.println(alpha);
+    }
+}
