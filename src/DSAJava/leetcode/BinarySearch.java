4f4896c5263958a461e543fb641c550d686c473e
ibrar talab <ibrartalabkhan@gmail.com>
2026-08-09 21:22:55 +0500

    Add recursiveBinarySearch solution as well.
    Signed-off-by: ibrar talab <ibrartalabkhan@gmail.com>

--- src/DSAJava/leetcode/BinarySearch.java
+++ src/DSAJava/leetcode/BinarySearch.java
@@ -6,6 +6,11 @@ public class BinarySearch {
         int[] nums = {-1,0,3,5,9,12};
         int result = binarySearch(nums,target);
         System.out.println(result);
+
+        int target2 = 14;
+        int[] nums2 = {2,3,5,6,7,11,12,14,18};
+        int recResult = recursiveBinarySearch(nums2,target2,0,nums2.length-1);
+        System.out.println(recResult);
     }
 
     public static int binarySearch(int[] nums,int target){
@@ -22,5 +27,18 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    public static int recursiveBinarySearch(int[] nums,int target,int low,int high){
+        if(low > high) return -1;
+        int mid = low + (high - low)/2;
+        if(nums[mid] == target){
+            return mid;
+        } else if (nums[mid] < target) {
+            return recursiveBinarySearch(nums,target,mid+1,high);
+        } else if (nums[mid] > target) {
+            return recursiveBinarySearch(nums,target,low,mid-1);
+        }
+        return recursiveBinarySearch(nums,target,low,high);
     }
 }
