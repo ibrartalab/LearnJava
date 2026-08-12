c258d702c2c55192b4ec907b380d80b06c457dae
ibrar talab <ibrartalabkhan@gmail.com>
2026-08-12 11:36:57 +0500

    Add the optimal solution for TwoSum problem using HashMap.
    Signed-off-by: ibrar talab <ibrartalabkhan@gmail.com>

--- src/DSAJava/leetcode/TwoSum.java
+++ src/DSAJava/leetcode/TwoSum.java
@@ -1,13 +1,21 @@
 package DSAJava.leetcode;
 
+import java.util.HashMap;
+
 public class TwoSum {
     static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 9;
-        int[] resultIndexes = TwoSum.findTwoSum(nums,target);
+        int[] resultIndexes = findTwoSum(nums,target);
         for (int index:resultIndexes){
             System.out.print(index + " ");
         }
+        System.out.println();
+        int[] resultIndexes2 = twoSumOptimal(nums,target);
+        for (int index:resultIndexes2){
+            System.out.print(index + " ");
+        }
+
     }
 
     // The Brute-Force(BF) solution
@@ -20,5 +28,22 @@ public class TwoSum {
             }
         }
         return new int[] {-1,-1};
+    }
+
+    // The optimal (better efficiency) solution
+    public static int[] twoSumOptimal(int[] nums,int target){
+        int i = 0;
+        int n = nums.length-1;
+        HashMap<Integer,Integer> numsMap = new HashMap<>();
+        while (i <= n){
+            int complement = target - nums[i];
+            if(numsMap.containsKey(complement)){
+                return new int[] {numsMap.get(complement),i};
+            }else{
+                numsMap.put(nums[i],i);
+            }
+            i++;
+        }
+        return new int[]{-1,-1};
     }
 }
