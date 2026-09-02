c9ff312e56723f7643df7e5a5529dd394c9e22d9
ibrar talab <ibrartalabkhan@gmail.com>
2026-09-02 18:44:41 +0500

    feat(projects): add repository methods to store students

---
+++ src/coreJava/projects/studentManagmentSystem/src/com/backend/repositories/StudentRepository.java
@@ -0,0 +1,37 @@
+package coreJava.projects.studentManagmentSystem.src.com.backend.repositories;
+
+import coreJava.projects.studentManagmentSystem.src.com.backend.models.Student;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class StudentRepository {
+    // This is the in-memory db to stores all the students data
+    // Encapsulation applied here, to protect our Student's data.
+    private final HashMap<Integer, Student> students = new HashMap<>();
+
+    /**
+     * This method will add a new student to the students list
+     * @param student its take the Student itself as s param to add into the students list
+     */
+    public void addStudent(Student student){
+        // edge cases checking
+        if(student != null){
+            int studentId = student.getId();
+            students.put(studentId,student);
+            System.out.println("Student " + student.getName() + " has been created!");
+        }else{
+            // tell the user there is something missing, please verify and retry again.
+            System.out.println("Not a valid Student Info!");
+        }
+    }
+
+    /**
+     * Get all the Students records by using this public method
+     * @return Return a collection of Student, to make sure you can apply all the collections
+     * methods
+     */
+    public Collection<Student> getAllStudents(){
+        return students.values();
+    }
+}
